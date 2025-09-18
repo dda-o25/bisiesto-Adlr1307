9143dac5f5601c4e1ebc06a0e94f10321f4efaf4
Adlr1307 <andres.jaime@iteso.mx>
2025-09-17 20:22:09 -0600

    Resolver año secular no bisiesto

--- bisiesto.py
+++ bisiesto.py
@@ -7,9 +7,12 @@ Andrés Enrique Jaime de la Rosa, 763799
 # Entradas
 año = int(input("Introduzca un año: "))
 
-if año % 4 == 0:
+
+if año % 400 == 0:
     print(año,"sí es un año bisiesto")
-elif año % 400 == 0:
+elif año % 100 == 0:
+    print(año,"no es un año bisiesto")
+elif año % 4 == 0:
     print(año,"sí es un año bisiesto")
 else:
     print(año,"no es un año bisiesto")
